71edefaeb90dc013c2bd1fea9903789960c96620
Angi Marliu Casella <acasella@danielcastelao.org>
2023-01-25 09:16:21 +0100

    3er Commit Colaborador

--- src/ColaboradorAngi.java
+++ src/ColaboradorAngi.java
@@ -2,4 +2,6 @@ public class ColaboradorAngi {
     //Realizo una modificación en el código y hago 1er. commit
 
     //Realizo segunda modificación en el código y hago 2do. commit
+
+    //Realizo tercera modificación en el código y hago 3er. commit
 }
